02c52685e2d569b35c846fa06a615edf5df0e995
Tomer Elmalem <telmalem@gmail.com>
2012-06-14 14:34:41 -0400

    Create the run method and print out a nested listing of the artist and albums folders.

--- rename_music_folders.rb
+++ rename_music_folders.rb
@@ -4,4 +4,20 @@ class Rename
   def get_folders
     Dir.entries(directory)
   end
+
+  def run
+    artists = self.get_folders
+    artists.each do |artist|
+      next if artist == "." || artist == ".."
+      artist_dir = directory + "/" + artist
+      puts "| #{artist}"
+      if File.directory?(artist_dir)
+        albums = Dir.entries(artist_dir)
+        albums.each do |album|
+          next if album == "." || album == ".."
+          puts "|-- #{album}"
+        end
+      end
+    end
+  end
 end
